511ee34fd77c547b126639dae77a548ebacc7a35
Mari Carmen <mdclg3@um.es>
2024-10-21 19:32:26 +0200

    Semana 7: Servlets

---
+++ ejemplos-persistencia-presentacion/src/main/java/aadd/servlets/TestServlet.java
@@ -0,0 +1,50 @@
+package aadd.servlets;
+
+import java.io.IOException;
+
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class TestServlet
+ */
+@WebServlet("/TestServlet")
+public class TestServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+    /**
+     * Default constructor. 
+     */
+    public TestServlet() {
+        // TODO Auto-generated constructor stub
+    }
+
+    public void init(ServletConfig config) throws ServletException {
+    	super.init(config);
+    	System.out.println("Entra en init");
+    }
+    
+    protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException{
+    	System.out.println("Entra a service");
+    	super.service(request, response);
+    }
+    
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		System.out.println("Entra a doGet");
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		System.out.println("Entra a doPost");
+	}
+
+}
